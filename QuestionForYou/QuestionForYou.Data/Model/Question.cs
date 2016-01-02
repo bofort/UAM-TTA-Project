ca4b7a71fcd74c68e9c5d570d163be17db265cec
bofort <morawskim92@gmail.com>
2016-01-02 12:26:05 +0100

    Add Untracked files

---
+++ QuestionForYou/QuestionForYou.Data/Model/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionForYou.Data.Model
+{
+    public class Question : ModelBase
+    {
+        public string Text { get; set; }
+        public Category Category { get; set; }
+        public ICollection<Answer> Answers { get; set; }
+    }
+}
---
+++ QuestionForYou/QuestionForYou.Data/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionForYou.Data.Model
+{
+    public class User : ModelBase
+    {
+        public string Name { get; set; }
+        public int Points { get; set; }
+        public bool HasQuestionToday { get; set; }
+    }
+}
