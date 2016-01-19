142f6c534645cb6277b62af22676ce06085bd3c4
l96s1 <patrykbern@gmail.com>
2016-01-19 11:03:48 +0100

    integration test - two test still fails

--- QuestionForYou/QuestionForYou.Tests.Integration/Storage/RepositoryTests.cs
+++ QuestionForYou/QuestionForYou.Tests.Integration/Storage/RepositoryTests.cs
@@ -39,6 +39,104 @@ namespace QuestionForYou.Tests.Integration.Storage
         {
             _scope.Dispose();
         }
+        [Test]
+        public void FindById_Should_Return_Null_When_Object_Og_Given_Id_Was_Not_Found()
+        {
+            var actual = _sut.FindById(4475438);
+
+            Assert.That(actual, Is.Null);
+        }
+
+        [Test]
+        public void GetAll_Returns_All_Items()
+        {
+            var model1 = new Question { Id = null, Text = "Who win super bowl", Category=new Category { Name = "Sport" } };
+            var model2 = new Question { Id = null, Text = "Wich team w", Category = new Category { Name = "Sport" } };
+
+            _sut.Persist(model1);
+            _sut.Persist(model2);
+            var result = _sut.GetAll();
+
+            Assert.That(result.Count(), Is.EqualTo(2));
+            CollectionAssert.AllItemsAreUnique(result);
+        }
+
+        [Test]
+        public void Persist_Should_Return_Copy_Of_Transient_Object_With_Id_Assigned()
+        {
+            var someTransientModel = new Question { Id = null, Text = "Who win super bowl", Category = new Category { Name = "Sport" } };
+
+            var result = _sut.Persist(someTransientModel);
+
+            Assert.That(result, Is.Not.Null);
+
+            Assert.That(result, Is.Not.SameAs(someTransientModel));
+            Assert.That(result.Text, Is.EqualTo(someTransientModel.Text));
+            Assert.That(result.Category, Is.EqualTo(someTransientModel.Category));
+            Assert.That(result.Id.HasValue);
+        }
+
+        [Test]
+        public void Persisted_Data_Should_Be_Accesible_By_Id_Via_FindById()
+        {
+            var someTransientModel = new Question { Id = null, Text = "Who win super bowl", Category = new Category { Name = "Sport" } };
+
+            var persisted = _sut.Persist(someTransientModel);
+            var actual = _sut.FindById(persisted.Id.Value);
+
+            Assert.That(actual.Id, Is.EqualTo(persisted.Id));
+            Assert.That(actual.Text, Is.EqualTo(persisted.Text));
+            Assert.That(actual.Category, Is.EqualTo(persisted.Category));
+        }
+
+        [Test]
+        public void Persisted_Object_With_Already_Existing_Id_Should_Evict_Previus_Data()
+        {
+            var someTransientModel = new Question { Id = null, Text = "Who win super bowl", Category = new Category { Name = "Sport" } };
+
+            var persisted = _sut.Persist(someTransientModel);
+            var anotherWithSameId = new Question { Id = persisted.Id, Text = "Who is the tallest NBA player", Category = new Category { Name = "Sport" } };
+            _sut.Persist(anotherWithSameId);
+            var actual = _sut.FindById(persisted.Id.Value);
+
+            Assert.That(actual.Id, Is.EqualTo(persisted.Id));
+            Assert.That(actual.Text, Is.EqualTo(anotherWithSameId.Text));
+            Assert.That(actual.Category, Is.EqualTo(anotherWithSameId.Category));
+        }
+
+        [Test]
+        public void Remove_Should_Remove_Item_Of_Same_Id_From_Storage()
+        {
+            var someTransientModel = new Question { Id = null, Text = "Who win super bowl", Category = new Category { Name = "Sport" } };
+
+            var persisted = _sut.Persist(someTransientModel);
+            var anotherWithSameId = new Question { Id = persisted.Id };
+            _sut.Remove(anotherWithSameId);
+
+            var actual = _sut.FindById(persisted.Id.Value);
+
+            Assert.That(actual, Is.Null);
+        }
+
+        [Test]
+        public void Subsequent_Persist_Calls_Objects_Should_Assign_Different_Id()
+        {
+            var someTransientModel = new Question { Id = null, Text = "Who win super bowl", Category = new Category { Name = "Sport" } };
+            var anotherTransientModel = (Question)someTransientModel.Clone();
+
+            var result1 = _sut.Persist(someTransientModel);
+            var result2 = _sut.Persist(anotherTransientModel);
+
+            Assert.That(result1, Is.Not.Null);
+            Assert.That(result2, Is.Not.Null);
+
+            Assert.That(result1, Is.Not.SameAs(someTransientModel));
+            Assert.That(result2, Is.Not.SameAs(someTransientModel));
+            Assert.That(result1, Is.Not.SameAs(result2));
+            Assert.That(result1.Id, Is.Not.Null);
+            Assert.That(result2.Id, Is.Not.Null);
+            Assert.That(result1.Id, Is.Not.EqualTo(result2.Id));
+        }
 
 
 
