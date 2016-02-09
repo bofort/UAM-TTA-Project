c123ae77f6cde2bf6230c128da3f97ba64bbc77f
bofort <morawskim92@gmail.com>
2016-02-09 15:11:38 +0100

    Commit uncommited changes

---
+++ QuestionForYou/QuestionForYou.Data/Storage/MigrationDB.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Data.Entity.Migrations;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace QuestionForYou.Data.Storage
+//{
+//    class MigrationDB : DbMigrationsConfiguration<QuestionForYouContext>
+//    {
+//        public MigrationDB()
+//        {
+//            AutomaticMigrationsEnabled = false;
+//        }
+
+//        protected override void Seed(QuestionForYouContext context)
+//        {
+//        }
+//    }
+//}
